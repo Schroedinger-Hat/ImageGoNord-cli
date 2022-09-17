de4e0f7a5353d56dc94db91746240bb941c06042
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2022-09-17 10:50:49 +0200

    Refactoring test

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -16,18 +16,11 @@ class ClientShould(unittest.TestCase):
         # Run the script with the image path input and output and check result
         with tempfile.TemporaryDirectory() as tmpdirname:
             output_image_path = Path(tmpdirname) / 'output.png'
-            command, output = run_image_go_nord_client(f'--img={self.blue_square_path}', f'--out={output_image_path}')
 
-            self.assertFalse(is_image_empty(output_image_path))
-
-            self.assertTrue(
-                are_images_the_same(self.blue_nord_square_path, output_image_path),
-                f"FAIL: The output image is NOT the same as the expected image \n{command}",
-            )
-
-            self.assertFalse(
-                are_images_the_same(self.blue_square_path, output_image_path),
-                f"FAIL: The output image IS the same as the expected image \n{command}",
+            self.run_test(
+                output_image_path,
+                f'--img={self.blue_square_path}',
+                f'--out={output_image_path}'
             )
 
     def test_convert_to_nord_palette_when_given_only_img_and_out_parameters_in_short_version(self):
