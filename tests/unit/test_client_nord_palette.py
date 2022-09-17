cdaab33a77574b559935348ee86ec637db878690
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2022-09-17 13:03:53 +0200

    Test added: Nord polarnight, aurora palette

--- tests/unit/test_client_nord_palette.py
+++ tests/unit/test_client_nord_palette.py
@@ -38,3 +38,18 @@ class ClientShould(UnitTestBaseClass):
                 f'-o={output_image_path}',
                 '--nord=Frost'
             )
+
+    def test_convert_to_nord_palette_with_polarnight_and_aurora_theme_when_given_specific_palette(self):
+        # Create a temporary folder to store the output image
+        # Run the script with the image path input and output and check result
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            output_image_path = Path(tmpdirname) / 'output.png'
+
+            self.run_test(
+                self.input_image_path,
+                output_image_path,
+                self.data / 'blue_nord_polarnight_aurora_square.png',
+                f'-i={self.input_image_path}',
+                f'-o={output_image_path}',
+                '--nord=PolarNight,Aurora'
+            )
