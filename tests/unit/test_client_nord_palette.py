0375559a80431407847be1d16814ce28fd41c3de
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2022-09-17 12:49:33 +0200

    Test added: Nord frost palette

--- tests/unit/test_client_nord_palette.py
+++ tests/unit/test_client_nord_palette.py
@@ -20,5 +20,21 @@ class ClientShould(UnitTestBaseClass):
                 output_image_path,
                 self.data / 'blue_nord_aurora_square.png',
                 f'-i={self.input_image_path}',
-                f'-o={output_image_path}', '--nord=Aurora'
+                f'-o={output_image_path}',
+                '--nord=Aurora'
+            )
+
+    def test_convert_to_nord_palette_with_frost_theme_when_given_specific_palette(self):
+        # Create a temporary folder to store the output image
+        # Run the script with the image path input and output and check result
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            output_image_path = Path(tmpdirname) / 'output.png'
+
+            self.run_test(
+                self.input_image_path,
+                output_image_path,
+                self.data / 'blue_nord_frost_square.png',
+                f'-i={self.input_image_path}',
+                f'-o={output_image_path}',
+                '--nord=Frost'
             )
