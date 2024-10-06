60673b4b55678c2c6f7792e0877eaa51429fe697
BugliL <bugli.lorenzo@gmail.com>
2024-10-06 20:26:56 +0200

    test: long blur parameter

--- tests/unit/mocked/test_client_advanced_behavior.py
+++ tests/unit/mocked/test_client_advanced_behavior.py
@@ -1,6 +1,6 @@
 from unittest.mock import ANY
-from image_go_nord_client.main import main
 
+from image_go_nord_client.main import main
 
 from .unit_test_base_class import UnitTestBaseClass
 
@@ -30,6 +30,15 @@ class ClientShould(UnitTestBaseClass):
         self.mock_gn_instance.disable_avg_algorithm.assert_called_once()
         self.mock_gn_instance.convert_image.assert_called_with(
             ANY, save_path="output.jpg"
+        )
+
+    def test_convert_to_nord_palette_using_long_blur_parameter(self):
+        main(argv=["image-go-nord-client", "--blur", "-i=file3.png"])
+        self.mock_gn_instance.disable_avg_algorithm.assert_not_called()
+        self.mock_gn_instance.enable_gaussian_blur.assert_called_once()
+        self.mock_gn_instance.open_image.assert_called_with("file3.png")
+        self.mock_gn_instance.convert_image.assert_called_with(
+            ANY, save_path=self.DEFAULT_OUTPUT_FILE_NAME
         )
 
     def test_convert_to_nord_palette_using_short_blur_parameter(self):
